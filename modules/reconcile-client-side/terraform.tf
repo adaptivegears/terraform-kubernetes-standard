62bbb1453e515c1d8c8a9ca9556159c67b601e9a
Andrey Gubarev <andrey@andreygubarev.com>
2025-06-23 00:58:06 +0400

    enable helm configuration in kustomize overlay

--- modules/reconcile-client-side/terraform.tf
+++ modules/reconcile-client-side/terraform.tf
@@ -2,4 +2,9 @@ data "kustomization_overlay" "this" {
   resources = [
     var.kustomization
   ]
+
+  kustomize_options = {
+    enable_helm = var.helm_enable
+    helm_path   = var.helm_path
+  }
 }
--- modules/reconcile-client-side/variables.tf
+++ modules/reconcile-client-side/variables.tf
@@ -3,3 +3,15 @@ variable "kustomization" {
   type        = string
   default     = "."
 }
+
+variable "helm_enable" {
+  description = "Enable Helm support in Kustomize."
+  type        = bool
+  default     = true
+}
+
+variable "helm_path" {
+  description = "The path to the Helm chart directory."
+  type        = string
+  default     = "helm"
+}
