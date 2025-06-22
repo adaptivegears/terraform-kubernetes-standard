aef68be13b2df37a708c1bf274aeeb978d5c4f5a
Andrey Gubarev <andrey@andreygubarev.com>
2025-06-23 01:53:42 +0400

    migrate kustomization resources to use build data

--- modules/reconcile-client-side/terraform.tf
+++ modules/reconcile-client-side/terraform.tf
@@ -8,3 +8,42 @@ data "kustomization_build" "this" {
     helm_path   = var.helm_path
   }
 }
+
+resource "kustomization_resource" "p0" {
+  for_each = data.kustomization_build.this.ids_prio[0]
+
+  manifest = (
+    contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
+    ? sensitive(data.kustomization_build.this.manifests[each.value])
+    : data.kustomization_build.this.manifests[each.value]
+  )
+}
+
+resource "kustomization_resource" "p1" {
+  for_each = data.kustomization_build.this.ids_prio[1]
+
+  manifest = (
+    contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
+    ? sensitive(data.kustomization_build.this.manifests[each.value])
+    : data.kustomization_build.this.manifests[each.value]
+  )
+  wait = true
+  timeouts {
+    create = "2m"
+    update = "2m"
+  }
+
+  depends_on = [kustomization_resource.p0]
+}
+
+resource "kustomization_resource" "p2" {
+  for_each = data.kustomization_build.this.ids_prio[2]
+
+  manifest = (
+    contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
+    ? sensitive(data.kustomization_build.this.manifests[each.value])
+    : data.kustomization_build.this.manifests[each.value]
+  )
+
+  depends_on = [kustomization_resource.p1]
+}
